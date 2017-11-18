5c96b4c82ea5ad2d277fc9eb74595ad7b1536bd6
Melissa Majewski <mreece85@gmail.com>
2017-11-18 14:07:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,5 @@
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
-
   puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
   puts "-----------"
   puts " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
@@ -12,6 +10,11 @@ end
 def input_to_index(user_input)
   user_input = user_input.to_i
   user_input = user_input - 1
+end
+
+def valid_move?(board, index)
+    if !position_taken?(board, index) && index.between?(0, 8)
+      true
 end
 
 def move(board, position, char='X')
@@ -25,9 +28,19 @@ def position_taken?(board, position)
     return true
   end
 
-def valid_move?(board, index)
-    !position_taken?(board, index) && index.between?(0, 8)
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valide_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
+
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
    board[index] ==  "" || board[index] == " "
